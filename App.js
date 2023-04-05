7eddeefd7da5f19f5d4d9fc918797be486e33597
nareshkumhar619 <nareshkumhar.619@gmail.com>
2023-04-05 22:15:58 +0530

    commit upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Temp from './componant/Weather/temp';
+
+// import Resturant from"./componant/Basics/Resturant";
+import Todo from './componant/ToDoReact/Todo';
+import Resturant from './componant/Basics/Resturant';
+// import UseEffect from './componant/hooks/useEffect';
+// import UseReducer from './componant/hooks/useReducer';
+// // import UseState from './componant/hooks/useState';
+
+
+
+const App = () => {
+  return (
+        <Todo/>
+  )
+}
+
+export default App ;
